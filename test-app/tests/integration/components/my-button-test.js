4ecb1912b600c2fef1b4d7cad41dce7ee7bb4bb1
Stanislav Dunajcan <sdunajcan@bmcgroup.com>
2023-04-03 14:46:21 +0200

    Fixed my-button test

--- test-app/tests/integration/components/my-button-test.js
+++ test-app/tests/integration/components/my-button-test.js
@@ -6,7 +6,8 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | my-component', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function () {
+  test('it renders', async function (assert) {
+    assert.expect(0);
     await render(hbs`<MyButton />`);
   });
 });
